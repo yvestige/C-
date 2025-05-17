2399b8c24b9d14c0bb3ec7bc6f0d502fd9f5d11e
yvbal <70612985+Yves242@users.noreply.github.com>
2025-05-17 23:07:26 +0800

    word-to-number: added values, iteration for word

--- 4. word-to-number.cpp
+++ 4. word-to-number.cpp
@@ -6,55 +6,54 @@
 #include <map>
 #include <cctype>
 
-std::map<std::string,std::map<std::string,std::string>> getMapTable() {
+/// @brief Function that returns the map table.
+/// @return A map of form {<value/multiplier>, {<identifier>,<int_value>}}
+std::map<std::string,std::map<std::string,long long>> getMapTable() {
 
     // make the map table
-    std::map<std::string,std::map<std::string,std::string>> mapTable = {
+    std::map<std::string,std::map<std::string,long long>> mapTable = {
         {
-            "numeric", {
-                {"one", "1"},
-                {"two", ""},
-                {"three", ""},
-                {"four", ""},
-                {"five", ""},
-                {"six", ""},
-                {"seven", ""},
-                {"eight", ""},
-                {"nine", ""},
-                {"ten", ""},
-                {"eleven", ""},
-                {"twelve", ""},
-                {"thirteen", ""},
-                {"fourteen", ""},
-                {"fifteen", ""},
-                {"sixteen", ""},
-                {"seventeen", ""},
-                {"eighteen", ""},
-                {"nineteen", ""},
-                {"twenty", ""},
-                {"thirty", ""},
-                {"forty", ""},
-                {"fifty", ""},
-                {"sixty", ""},
-                {"seventy", ""},
-                {"eighty", ""},
-                {"ninety", ""},
+            "value", {
+                {"one", 1},
+                {"two", 2},
+                {"three", 3},
+                {"four", 4},
+                {"five", 5},
+                {"six", 6},
+                {"seven", 7},
+                {"eight", 8},
+                {"nine", 9},
+                {"ten", 10},
+                {"eleven", 11},
+                {"twelve", 12},
+                {"thirteen", 13},
+                {"fourteen", 14},
+                {"fifteen", 15},
+                {"sixteen", 16},
+                {"seventeen", 17},
+                {"eighteen", 18},
+                {"nineteen", 19},
+                {"twenty", 20},
+                {"thirty", 30},
+                {"forty", 40},
+                {"fifty", 50},
+                {"sixty", 60},
+                {"seventy", 70},
+                {"eighty", 80},
+                {"ninety", 90},
             }
         },
+
         {
             "multiplier", {
-                {"hundred",""},
-                {"thousand",""},
-                {"million",""},
-                {"billion",""},
-                {"trillion",""},
-                {"quadrillion",""},
-                {"quintillion",""},
-                {"sextillion",""},
-                {"septillion",""},
-                {"octillion",""},
-                {"nonillion",""},
-                {"",""},
+                {"hundred",100},
+                {"thousand",1000},
+                {"million",1000000},
+                {"billion",1000000000},
+                {"trillion",1000000000000},
+                {"quadrillion",1000000000000000},
+                {"quintillion",1000000000000000000},
+                {"negative",-1},
             }
         }
     };
@@ -66,7 +65,7 @@ std::map<std::string,std::map<std::string,std::string>> getMapTable() {
 /// @brief Function that converts a string of words into a list of strings
 /// @param words stringed words that will be tokenized.
 /// @return Vector of string words
-std::vector<std::string> getTokenWordsVector(const std::string& words) {
+std::vector<std::string> turnWordsToWordVector(const std::string& words) {
 
     // variables
     std::istringstream wordStream(words);
@@ -98,9 +97,42 @@ std::vector<std::string> getTokenWordsVector(const std::string& words) {
 void convertWordToNumber(const std::string& words) {
     
     // acquire vector of strings
-    std::vector<std::string> wordVector = getTokenWordsVector(words);
+    std::vector<std::string> wordVector = turnWordsToWordVector(words);
 
+    // get the map table
+    const auto& mapTable = getMapTable();
 
+    // assign references
+    const auto& valueMap = mapTable.at("value");
+    const auto& multiplierMap = mapTable.at("multiplier");
+
+    // variables for reference in computation
+    long long value = 0, multiplier = 1;
+    bool isNegative = false;
+
+    // iterate through each word
+    for (const auto& word : wordVector) {
+
+        // if negative, set bool switch and skip to next
+        if (word == "negative") {
+            isNegative = true;
+            continue;
+        }
+
+        // if word is a value
+        if (valueMap.find(word) != valueMap.end()) {
+            // do algorithm for value
+        
+
+        // if word is a multiplier
+        } else if (multiplierMap.find(word) != multiplierMap.end()) {
+            // do method for multiplier
+        
+        // if none found
+        } else {
+            std::cout << "ERROR: token '" << word << "' invalid.\n"; 
+        }
+    }
 }
 
 // main function
@@ -123,4 +155,6 @@ int main() {
         std::cout << "\n";
     }
 
+    // actually return something
+    return 0;
 }
